d9eccebf5347dcdea7636fef25eaca79ec476a9b
LinBq <bq_lin@hotmail.com>
2017-08-24 15:25:51 +0800

    创建 PLVPlayer 测试项目

---
+++ PolyvAVPlayerExp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PolyvAVPlayerExp
+//
+//  Created by LinBq on 2017/8/24.
+//  Copyright © 2017年 Bq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
